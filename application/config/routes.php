fe448593fbfa46b1e2f30e85a9a230cf62ab169c
johnyors <royfdc@gmail.com>
2016-05-12 11:12:51 +0800

    htaccess

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+$route['sample'] = 'welcome/sample';
+
+$route['admin'] = 'admin/AdminHomepageController';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/controllers/admin/AdminHomepageController.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminHomepageController extends CI_Controller {
+    
+    public function index() {
+        echo "this is admin";
+    }
+}
